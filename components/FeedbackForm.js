fe47ab10208c3484c62a877e7df2c42d9b2f2540
nickmvp1 <114506332+nickmvp1@users.noreply.github.com>
2022-09-27 12:17:34 -0400

    Update FeedbackForm.js

--- components/FeedbackForm.js
+++ components/FeedbackForm.js
@@ -2,30 +2,25 @@ import styles from './FeedbackForm.module.css'
 
 export default function FeedbackForm() {
   return (
-      <form
-        className={styles.form}
-        data-netlify="true"
-        data-netlify-honeypot="bot-field"
-        name="feedback"
-        method="POST"
-        action="/success"
-      >
-        <input type="hidden" name="form-name" value="feedback" />
-        <p className={styles.hidden}>
-            <label>
-            Don’t fill this out if you’re human: <input name="bot-field" />
-            </label>
-        </p>
-  
-        <label htmlFor="name">Name</label>
-        <input id="name" className={styles['form-field']} type="text" name="name" />
-
-        <label htmlFor="email">Email</label>
-        <input id="email" className={styles['form-field']} type="email" name="email" required />
-
-        <label htmlFor="feedback">What is your feedback?</label>
-        <textarea id="feedback" className={styles['form-field']} wrap="soft" name="feedback" required></textarea>
-        <button className={styles.button} type="submit">Submit</button>
-      </form>
+<form name="contact" method="POST" data-netlify="true">
+  <p>
+    <label>Your Name: <input type="text" name="name" /></label>
+  </p>
+  <p>
+    <label>Your Email: <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <label>Your Role: <select name="role[]" multiple>
+      <option value="leader">Leader</option>
+      <option value="follower">Follower</option>
+    </select></label>
+  </p>
+  <p>
+    <label>Message: <textarea name="message"></textarea></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
   )
 }
